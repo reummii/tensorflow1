bb40bbcb933bb027f19557e6c6ef34270307c60c
reummii <jureum9@gmail.com>
2020-05-26 10:15:30 +0900

    Create tensorflowtest.py

---
+++ tensorflowtest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+
+
+# In[5]:
+
+
+node1 = tf.constant(3.0,dtype=tf.float32)
+
+
+# In[6]:
+
+
+node2=tf.constant(4.0)
+
+
+# In[7]:
+
+
+tf.print(node1,node2)
+
+
+# In[ ]:
+
+
+
+
